cd1744d4a2eb25482abf4df384e18bee98985381
Casper <casper.koch.dev@gmail.com>
2022-10-26 12:07:53 +0200

    Added MQTT

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -1,9 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <MQTT.h>
 
+const char ssid[] = "SibirienAP";
+const char pass[] = "Siberia51244";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "mangosting", "CXzamZsmmy1iSedX")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
+  // by Arduino. You need to set the IP address directly.
+  client.begin("mangosting.cloud.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  client.loop();
 
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
 }
